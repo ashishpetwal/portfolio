5286a95571321676efa70dadab2267e73b26ef65
Ashish <ashishpetwalap2112@gmail.com>
2023-02-14 23:37:36 +0530

    "Added SetInterval for Headings"

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let hamBtn = document.getElementsByClassName("myBtn");
 let list = document.getElementById("list");
+let title = document.getElementById("name-title");
 
 const handleClick = () =>{
     if(list.style.display == "block"){
@@ -9,3 +10,10 @@ const handleClick = () =>{
         list.style.display = "block";
     }
 }
+
+setInterval(() => {
+    title.innerText = "Designer";
+}, 7500);
+setInterval(() => {
+    title.innerText = "Developer";
+}, 15000);
